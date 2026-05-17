379eddc9bfbe1c3cc1090856e43208c917bf7208
Heitezy <heitezy@gmail.com>
2026-05-17 22:40:42 +0300

    bumped up version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         minSdk = 24
         //noinspection OldTargetApi
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.0.4"
+        versionCode = 13
+        versionName = "1.1.0"
     }
 
     buildFeatures {
