a68e0b87e3c9c969c6a2255e1fae49e896c1d6b1
Heitezy <heitezy@gmail.com>
2026-05-25 23:43:44 +0300

    bumped up version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         minSdk = 24
         //noinspection OldTargetApi
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.1.1"
+        versionCode = 15
+        versionName = "1.1.2"
     }
 
     buildFeatures {
@@ -98,6 +98,7 @@ tasks.register("license") {
 
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2026.04.01"))
+    implementation("androidx.compose.foundation:foundation-layout:1.11.2")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.activity:activity-compose:1.13.0")
