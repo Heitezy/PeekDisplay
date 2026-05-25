74c356201f6351d5dd999db83e1b70e058d936f5
Heitezy <heitezy@gmail.com>
2026-05-25 23:47:01 +0300

    removed unused dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,7 +98,6 @@ tasks.register("license") {
 
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2026.04.01"))
-    implementation("androidx.compose.foundation:foundation-layout:1.11.2")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.activity:activity-compose:1.13.0")
